32e48f8f757b8edabd115ccb09c8a8396caaf4a4
Ariana Faraday <arianaloreleifaraday@gmail.com>
2022-03-09 18:56:59 -0500

    starter envelopes

---
+++ db.js
@@ -0,0 +1,22 @@
+const envelopes = [
+    {
+        id: 1,
+        category: "groceries",
+        limit: 500,
+        balance: 500
+    },
+    {
+        id: 2,
+        category: "entertainment",
+        limit: 300,
+        balance: 300
+    },
+    {
+        id: 3,
+        category: "health",
+        limit: 150,
+        balance: 150
+    }
+]
+
+module.exports = envelopes;
